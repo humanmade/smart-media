4f65f163ecb3c895fdafbe4fa38d077493db69cf
Joe Hoyle <joehoyle@gmail.com>
2019-04-29 11:44:04 +0700

    v0.1.12

--- plugin.php
+++ plugin.php
@@ -4,7 +4,7 @@
  * Description: Advanced media tools that take advantage of Rekognition and Tachyon.
  * Author: Human Made Limited
  * License: GPL-3.0
- * Version: 0.1.11
+ * Version: 0.1.12
  */
 
 namespace HM\Media;
