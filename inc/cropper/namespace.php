8cf2a3cfe03564ec6236821fa265b6e7d37a5b04
Robert O'Rourke <robert@o-rourke.org>
2020-02-20 17:03:37 +0000

    double check attachment ID incase of upstream filter / bug

--- inc/cropper/namespace.php
+++ inc/cropper/namespace.php
@@ -147,6 +147,10 @@ function image_downsize( array $tachyon_args, array $downsize_args ) : array {
 	if ( ! isset( $downsize_args['attachment_id'] ) || ! isset( $downsize_args['size'] ) ) {
 		return $tachyon_args;
 	}
+	
+	if ( ! is_int( $downsize_args['attachment_id'] ) ) {
+		return $tachyon_args;
+	}
 
 	$crop = get_crop( $downsize_args['attachment_id'], $downsize_args['size'] );
 
