c484823a3150ef8ce31d4fcd46159b28cadb8bc1
Joe Hoyle <joe@humanmade.co.uk>
2020-05-06 18:53:34 +0200

    Update inc/cropper/namespace.php
    Co-authored-by: Ryan McCue <me@ryanmccue.info>

--- inc/cropper/namespace.php
+++ inc/cropper/namespace.php
@@ -333,7 +333,7 @@ function attachment_js( $response, $attachment ) {
 	}
 
 	// Add base Tachyon URL.
-	if ( function_exists( 'tachyon_url' ) && Tachyon::validate_image_url( $response['url'] ) ) {
+	if ( method_exists( 'Tachyon', 'validate_image_url' ) && Tachyon::validate_image_url( $response['url'] ) ) {
 		$response['original_url'] = $response['url'];
 		$response['url'] = tachyon_url( $response['url'] );
 	}
