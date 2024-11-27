d283a976cef54403ba29d26619e2cc27d8225129
Robert O'Rourke <23417+roborourke@users.noreply.github.com>
2024-11-27 14:35:59 +0000

    Tagging 0.5.6

--- plugin.php
+++ plugin.php
@@ -4,7 +4,7 @@
  * Description: Advanced media tools that take advantage of Rekognition and Tachyon.
  * Author: Human Made Limited
  * License: GPL-3.0
- * Version: 0.5.5
+ * Version: 0.5.6
  */
 
 namespace HM\Media;
